00015965c9566f94711a2fe4b4fe992fb5b57af9
hekalrapli <haikalrfli11@gmail.com>
2026-03-17 12:09:26 +0700

    haikal changes

--- source_code/src/App.java
+++ source_code/src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        System.out.println("There must be a good reason that you're gone");
     }
 }
