312f62504ae2eca88da8c1e7ce1bffbeba4b95a7
ariefinsulthon-cmyk <ariefinsulthon@gmail.com>
2026-03-28 17:28:33 +0700

    Loan & Costumer done(Sulthon Ganteng)

--- source_code/src/Customer.java
+++ source_code/src/Customer.java
@@ -1,3 +1,53 @@
 public class Customer {
-    
+  private String customerId;
+  private String name;
+  private String email;
+  private String phone;
+
+  /* Konstruktor */
+  public Customer() {}
+
+  /* Selektor */
+  public String getCustomerId() {
+    return customerId;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public String getPhone() {
+    return phone;
+  }
+
+  /* Mutator */
+  public void setCustomerId(String customerId) {
+    this.customerId = customerId;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public void setPhone(String phone) {
+    this.phone = phone;
+  }
+
+  /* Method Tambahan */
+  public void updateContact(String newEmail) {
+    this.email = newEmail;
+  }
+
+  public void updateContact(String newEmail, String newPhone) {
+    this.email = newEmail;
+    this.phone = newPhone;
+  }
 }
