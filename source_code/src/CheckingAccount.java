a4ccc82756efa477d5d8bb21442e9cb6f506af6b
“rioshp” <riohastanup@gmail.com>
2026-03-31 14:10:29 +0700

    menyelesaikan (checking,savings)-Account, IPaymentProcessor

--- source_code/src/CheckingAccount.java
+++ source_code/src/CheckingAccount.java
@@ -1,7 +1,49 @@
 public class CheckingAccount extends Account implements IPaymentProcessor {
-    
+    /* Atribut */
+    private double overdraftLimit;
 
+    /* Method */
+    public CheckingAccount(){
+        super();
+        overdraftLimit = 500000;
+    }
+
+    public CheckingAccount(String accountNumber, double balance, double overdraftLimit) {
+        super(accountNumber, balance);
+        this.overdraftLimit = overdraftLimit;
+    }
+
+    public double getOverdraftLimit() {
+        return overdraftLimit;
+    }
+
+    public void setOverdraftLimit(double overdraftLimit) {
+        this.overdraftLimit = overdraftLimit;
+    }
+
+    @Override
+    public void withdraw(double amount) throws Exception {
+        if (amount > overdraftLimit + getBalance()) {
+            throw new Exception("Penarikan gagal: Saldo dan Overdraft tidak cukup.");
+        }
+        super.withdraw(amount);
+    }
+    
+    @Override
     public double calculateInterest() {
         return 0;
     };
+
+    @Override
+    public boolean processPayment(double amount, String billerId) {
+        try {
+            this.withdraw(amount);
+            System.out.println("Pembayaran sebesar " + amount + " ke " + billerId + " berhasil.");
+            return true;
+        } catch (Exception e) {
+            System.out.println("Pembayaran gagal: Saldo dan Overdraft tidak cukup.");
+            return false;
+        }
+    }
+
 }
--- source_code/src/IPaymentProcessor.java
+++ source_code/src/IPaymentProcessor.java
@@ -1,4 +1,4 @@
 public interface IPaymentProcessor {
-
-    
+    /* Method */
+    boolean processPayment(double amount, String billerId);
 }
